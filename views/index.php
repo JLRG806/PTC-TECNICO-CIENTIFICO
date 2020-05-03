b182495d6ad44290c5fe6e5a9f26265f7a0658b0
Jose Luis <47016453+JLRG806@users.noreply.github.com>
2020-05-03 17:34:50 -0600

    Update index.php

--- views/index.php
+++ views/index.php
@@ -16,7 +16,7 @@
     <footer>
 
     </footer>
-    
-    <script src="../resources/js/bootstrap.js"></script>
+    <script src="../resources/js/jquery.min.js"></script>
+    <script src="../resources/js/bootstrap.min.js"></script>
 </body>
 </html>
