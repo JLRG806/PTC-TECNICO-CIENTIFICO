03682d216fccd3236b0f56f91583f7200b5d5b64
Jose Luis <47016453+JLRG806@users.noreply.github.com>
2020-05-03 17:26:00 -0600

    jquery

--- views/index.php
+++ views/index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Itr - App Estudiante</title>
+    <link rel="stylesheet" href="">
 </head>
 <body>
     <nav>
@@ -13,7 +14,9 @@
 
     </main>
     <footer>
-        
+
     </footer>
+    
+    <script src="../resources/js/bootstrap.js"></script>
 </body>
 </html>
