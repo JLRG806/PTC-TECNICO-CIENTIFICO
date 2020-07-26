6a7076cf5aa60fe6590bae990628d715fb5b5b61
HolaAdios2105 <64664483+HolaAdios2105@users.noreply.github.com>
2020-07-26 17:54:09 -0600

    CRUD USUARIO listo

---
+++ core/api/usuario.php
@@ -0,0 +1,125 @@
+<?php
+require_once('../helpers/database.php');
+require_once('../helpers/validator.php');
+require_once('../models/usuarios.php');
+
+// Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
+if (isset($_GET['action'])) {
+	// Se instancia la clase correspondiente.
+	$usuario = new Usuarios;
+	// Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+	$result = array('status' => 0, 'message' => null, 'exception' => null);
+	// Se compara la acción a realizar cuando un administrador ha iniciado sesión.
+	switch ($_GET['action']) {
+		case 'search':
+            $_POST = $usuario->validateForm($_POST);
+			if ($_POST['usuario_buscar'] != '') {
+				if ($result['dataset'] = $usuario->searchUsuario($_POST['usuario_buscar'])) {
+					$result['status'] = 1;
+					$rows = count($result['dataset']);
+					if ($rows > 1) {
+						$result['message'] = 'Se encontraron '.$rows.' coincidencias';
+					} else {
+						$result['message'] = 'Solo existe una coincidencia';
+					}
+				} else {
+					$result['exception'] = 'No hay coincidencias';
+				}
+			} else {
+				$result['exception'] = 'Ingrese un valor para buscar';
+			}
+			break;
+            case 'create':
+                $_POST = $usuario->validateForm($_POST);
+                if ($usuario->setNombre($_POST['nombre'])) {
+                    if ($usuario->setCorreo($_POST['email'])) {
+                        if ($usuario->setEstado($_POST['estado'])) {
+                            if ($_POST['password'] == $_POST['password_con']) {
+                                if ($usuario->setPassword($_POST['password'])) {
+                                    if ($usuario->createUsuario()) {
+                                        $result['status'] = 1;
+                                        $result['message'] = 'Usuario creado correctamente';
+                                    } else {
+                                        $result['exception'] = Database::getException();
+                                    }
+                                } else {
+                                    $result['exception'] = 'Clave menor a 6 caracteres';
+                                }
+                            } else {
+                                $result['exception'] = 'Claves diferentes';
+                            }                        
+                        } else {
+                            $result['exception'] = 'Estado incorrecto';
+                        }
+                    } else {
+                        $result['exception'] = 'Correo incorrecto';
+                    }
+                } else {
+                    $result['exception'] = 'Nombre incorrecto';
+                }
+            break;
+		case 'readOne':
+			if ($usuario->setId($_POST['id_usuario'])) {
+				if ($result['dataset'] = $nivel->readOneUsuario()) {
+					$result['status'] = 1;
+				} else {
+					$result['exception'] = 'Nivel inexistente';
+				}
+			} else {
+				$result['exception'] = 'Nivel incorrecto';
+			}
+			break;
+		case 'readAll':
+			if ($result['dataset'] = $usuario->readAllUsuarios()) {
+				$result['status'] = 1;
+			} else {
+				$result['exception'] = 'Nivel inexistente';
+			}
+			break;
+            case 'update':
+                $_POST = $usuario->validateForm($_POST);
+                if ($usuario->setNombre($_POST['nombre'])) {
+                    if ($usuario->setCorreo($_POST['email'])) {
+                        if ($usuario->setEstado($_POST['estado'])) {
+                            if ($usuario->updateUsuario()) {
+                                $result['status'] = 1;
+                                $result['message'] = 'Usuario modificado correctamente';
+                            } else {
+                                $result['exception'] = Database::getException();
+                            }
+                        } else {
+                            $result['exception'] = 'Estado incorrecto';
+                        }
+                    } else {
+                        $result['exception'] = 'Correo incorrecto';
+                    }
+                } else {
+                    $result['exception'] = 'Nombre incorrecto';
+                }
+            break;
+		case 'delete':
+			if ($usuario->setId($_POST['id_usuario'])) {
+				if ($usuario->readOneUsuario()) {
+					if ($usuario->deleteUsuario()) {
+						$result['status'] = 1;
+						$result['message'] = 'Usario eliminado correctamente';
+					} else {
+						$result['exception'] = Database::getException();
+					}
+				} else {
+					$result['exception'] = 'Usuario inexistente';
+				}
+			} else {
+				$result['exception'] = 'Usuario incorrecto';
+			}
+			break;
+		default:
+			exit('Acción no disponible log');
+	}
+	// Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+	header('content-type: application/json; charset=utf-8');
+	// Se imprime el resultado en formato JSON y se retorna al controlador.
+	print(json_encode($result));
+} else {
+	exit('Recurso denegado');
+}
---
+++ core/controllers/usuario.js
@@ -0,0 +1,96 @@
+// Constante para establecer la ruta y parámetros de comunicación con la API.
+const API_USUARIO = '../core/api/usuario.php?action=';
+
+// Método que se ejecuta cuando el documento está listo.
+$( document ).ready(function() {
+    // Se llama a la función que obtiene los registros para llenar la tabla. Se encuentra en el archivo components.js
+    readRows( API_USUARIO );
+});
+
+// Función para llenar la tabla con los datos enviados por readRows().
+function fillTable( dataset )
+{
+    let content = '';
+    // Se recorre el conjunto de registros (dataset) fila por fila a través del objeto row.
+    dataset.forEach(function( row ) {
+        // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+        content += `
+            <tr>
+                <td>${row.nombre_usuario}</td>
+                <td>${row.email_usuario}</td>
+                <td>${row.estado_usuario}</td>
+                <td>
+                    <a href="#" onclick="openUpdateModal(${row.id_usuario})" class="blue-text tooltipped" data-tooltip="Actualizar"><i class="material-icons">mode_edit</i></a>
+                    <a href="#" onclick="openDeleteDialog(${row.id_usuario})" class="red-text tooltipped" data-tooltip="Eliminar"><i class="material-icons">delete</i></a>
+                </td>
+            </tr>
+        `;
+    });
+    // Se agregan las filas al cuerpo de la tabla mediante su id para mostrar los registros.
+    $( '#tbody-rows' ).html( content );
+}
+
+// Evento para mostrar los resultados de una búsqueda.
+$( '#buscar' ).submit(function( event ) {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se llama a la función que realiza la búsqueda. Se encuentra en el archivo components.js
+    searchRows( API_USUARIO, this );
+});
+
+// Función que prepara formulario para modificar un registro.
+function openUpdateModal( id )
+{
+    // Se limpian los campos del formulario.
+    $( '#USUARIOS' )[0].reset();
+
+    $.ajax({
+        dataType: 'json',
+        url: API_USUARIO + 'readOne',
+        data: { id_usuario: id },
+        type: 'post'
+    })
+    .done(function( response ){
+        // Se comprueba si la API ha retornado una respuesta satisfactoria, de lo contrario se muestra un mensaje de error.
+        if ( response.status ) {
+            // Se inicializan los campos del formulario con los datos del registro seleccionado previamente.
+            $( '#id_usuario' ).val( response.dataset.id_usuario );
+            $( '#nombre' ).val( response.dataset.nombre_usuario );
+            $( '#email' ).val( response.dataset.email_usuario );
+            $( '#estado' ).val( response.dataset.estado_usuario );
+            // Se actualizan los campos para que las etiquetas (labels) no queden sobre los datos.
+            //M.updateTextFields(); 
+        } else {
+            sweetAlert( 2, result.exception, null );
+        }
+    })
+    .fail(function( jqXHR ){
+        // Se verifica si la API ha respondido para mostrar la respuesta, de lo contrario se presenta el estado de la petición.
+        if ( jqXHR.status == 200 ) {
+            console.log( jqXHR.responseText );
+        } else {
+            console.log( jqXHR.status + ' ' + jqXHR.statusText );
+        }
+    });
+}
+
+// Evento para crear o modificar un registro.
+$( '#USUARIOS' ).submit(function( event ) {
+    event.preventDefault();
+    // Se llama a la función que crea o actualiza un registro. Se encuentra en el archivo components.js
+    // Se comprueba si el id del registro esta asignado en el formulario para actualizar, de lo contrario se crea un registro.
+    if ( $( '#id_usuario' ).val() ) {
+        saveRow( API_USUARIO, 'update', this, null );
+    } else {
+        saveRow( API_USUARIO, 'create', this, null );
+    }
+});
+
+// Función para establecer el registro a eliminar mediante el id recibido.
+function openDeleteDialog( id )
+{
+    // Se declara e inicializa un objeto con el id del registro que será borrado.
+    let identifier = { id_usuario: id };
+    // Se llama a la función que elimina un registro. Se encuentra en el archivo components.js
+    confirmDelete( API_USUARIO, identifier );
+}
