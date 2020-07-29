033c69d2ce20aa8f0a49a0366b71db860357d612
blurerp <fernandoblure15@gmail.com>
2020-07-29 12:14:35 -0600

    cambio

--- core/helpers/components.js
+++ core/helpers/components.js
@@ -258,8 +258,6 @@ function fillSelect( api, selectId, selected )
         } else {
             $( '#' + selectId ).html( '<option value="">No hay opciones disponibles</option>' );
         }
-        // Se inicializa el componente Select del formulario para que muestre las opciones.
-        $( 'select' ).formSelect();
     })
     .fail(function( jqXHR ) {
         // Se verifica si la API ha respondido para mostrar la respuesta, de lo contrario se presenta el estado de la petición.
